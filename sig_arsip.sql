273f7e9fd3bd3e2c2654970c5fe17a0bc7d7b0c8
rizki kurniawan <kiukiukurniawan@gmail.com>
2015-12-16 09:03:00 +0700

    update struktur db

--- sig_arsip.sql
+++ sig_arsip.sql
@@ -35,6 +35,7 @@ CREATE TABLE IF NOT EXISTS `arsip` (
   `tema` varchar(255) NOT NULL,
   `jabatan_id` int(11) NOT NULL,
   `penyimpanan_id` int(11) NOT NULL,
+  `jenis` varchar(255) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `perusahaan_id` (`perusahaan_id`),
   KEY `divisi_id` (`divisi_id`),
