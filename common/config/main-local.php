cac38caa55aa79d5f786ff40761f8c8d3dc23d46
root <root@jakarta.ppsdm.com>
2018-08-20 14:49:17 +0700

    add migration, still unifinished

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -4,8 +4,8 @@ return [
         'db' => [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=sigarsip',
-            'username' => 'root',
-            'password' => '',
+            'username' => 'ppsdm',
+            'password' => 'ppsdM2014',
             'charset' => 'utf8',
         ],
         'mailer' => [
---
+++ console/migrations/m180820_074150_arsip.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m180820_074150_arsip extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180820_074150_arsip cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
